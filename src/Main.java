844d8024dff853d1d3a783bf50df28d05b011c89
elena.denoeva <denoeva.elena@skyeng.ru>
2023-04-14 20:19:25 +0300

    Рефакторинг с учетом принципов ООП

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        Car car2 = new Car();
-        car.modelName = "car1";
-        car2.modelName = "car2";
-        car.wheelsCount = 4;
-        car2.wheelsCount = 4;
+        Car car = new Car("Skoda Kodiaq", 4);
+        Car car2 = new Car("Suzuki Jimny", 4);
 
-        Truck truck = new Truck();
-        Truck truck2 = new Truck();
-        truck.modelName = "truck1";
-        truck2.modelName = "truck2";
-        truck.wheelsCount = 6;
-        truck2.wheelsCount = 8;
+        Truck truck = new Truck("Volvo", 6);
+        Truck truck2 = new Truck("КамАЗ", 8);
 
-        Bicycle bicycle = new Bicycle();
-        Bicycle bicycle2 = new Bicycle();
-        bicycle.modelName = "bicycle1";
-        bicycle2.modelName = "bicycle2";
-        bicycle.wheelsCount = 2;
-        bicycle2.wheelsCount = 2;
+        Bicycle bicycle = new Bicycle("Аист",2 );
+        Bicycle bicycle2 = new Bicycle("Electra", 2);
 
-        ServiceStation station = new ServiceStation();
-        station.check(car, null, null);
-        station.check(car2, null, null);
-        station.check(null, bicycle, null);
-        station.check(null, bicycle2, null);
-        station.check(null, null, truck);
-        station.check(null, null, truck2);
+//        ServiceStation station = new ServiceStationTransport();
+//        station.check(car, null, null);
+//        station.check(car2, null, null);
+//        station.check(null, bicycle, null);
+//        station.check(null, bicycle2, null);
+//        station.check(null, null, truck);
+//        station.check(null, null, truck2);
+
+        ServiceStation serviceStationTransport = new ServiceStationTransport();
+        serviceStationTransport.check(car, bicycle, truck);
+
+//        checkTransport(serviceStationTransport, car, bicycle, truck2);
     }
+//    private static void checkTransport(ServiceStation serviceStation, Car car, Bicycle bicycle, Truck truck) {
+//        serviceStation.check(car, bicycle, truck);
+//    }
 }
